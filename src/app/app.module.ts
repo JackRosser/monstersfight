8e832fd3056cd3a0e49dfea845ea25f58f7d4e9f
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-14 14:03:13 +0200

    it works!!!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Route, RouterModule } from '@angular/router';
+import { MonsterselectComponent } from './pages/monsterselect/monsterselect.component';
+
+const routes: Route[] = [
+  {
+    path: "",
+    component: MonsterselectComponent
+
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MonsterselectComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/i-monsters.ts
@@ -0,0 +1,16 @@
+export interface iMonsters {
+  id: number,
+  name: string,
+  type: string,
+  hp: number,
+  stamina: number,
+  atk: number,
+  def: number,
+  speed: number,
+  img: string,
+  icon: string,
+  description: string,
+  principale: string,
+  secondario: string,
+  debolezza: string
+}
