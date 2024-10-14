844901e35fea61f9e25505ca6108828b1c1d9840
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-14 16:17:11 +0200

    cards

--- src/app/pages/monsterselect/monsterselect.component.ts
+++ src/app/pages/monsterselect/monsterselect.component.ts
@@ -1,3 +1,4 @@
+import { iMonsters } from './../../models/i-monsters';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrl: './monsterselect.component.scss'
 })
 export class MonsterselectComponent {
+
+// AUDIO
+musicMenu: string = "public/services/musics/MenuMusic.mp3"
+
+monstersList!: iMonsters[]
+monsterPic!: string
+backImage!: string
+
+scorrimento(): void {
+
+}
+
+ngOnInit() {
+  fetch("monsters.json").then(res => {
+    if (!res.ok) {
+      throw new Error("Errore nella chiamata")
+    }
+    return res.json()
+  }).then((data:iMonsters[]) => {
+    this.monstersList = data
+    if(this.monstersList.length > 0) {
+this.monsterPic = this.monstersList[0].img
+this.backImage = `background-image: url(${this.monsterPic})`
+    }
+
+
+
+  }).catch(err => {
+    console.log("ERRORE:", err);
+
+  })
+
+}
 
 }
