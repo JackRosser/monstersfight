9cb75e7b76e4e5c74ae1aa90fa6725a993a2e8cd
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-16 09:16:07 +0200

    fix inizio mattina

--- src/app/pages/previews/previews.component.ts
+++ src/app/pages/previews/previews.component.ts
@@ -14,30 +14,6 @@ export class PreviewsComponent {
 @Input() cardInDeck!: iMonsters
 @Input() inputDeck!: (card: iMonsters) => void
 
-acqua:boolean = false
-fuoco:boolean = false
-vento:boolean = false
-terra:boolean = false
-erba:boolean = false
-legendary:boolean = false
-monstersFilter!: iMonsters[]
-
-showImagesSelected(): void {
-  if (this.acqua) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "acqua");
-  } else if (this.fuoco) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "fuoco");
-  } else if (this.vento) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "vento");
-  } else if (this.terra) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "terra");
-  } else if (this.erba) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "erba");
-  } else if (this.legendary) {
-    this.monstersFilter = this.monstersList.filter(type => type.type === "legendary");
-  }
-}
-
 
 
 }
--- src/app/pages/selectattribute/selectattribute.component.ts
+++ src/app/pages/selectattribute/selectattribute.component.ts
@@ -10,49 +10,6 @@ export class SelectattributeComponent {
 icons: string[] = ["services/icons/acqua.webp", "services/icons/fuoco.webp", "services/icons/vento.webp", "services/icons/terra.webp", "services/icons/erba.webp", "services/icons/legendary.webp"]
 iconClass: string = "max-h-[4rem] rounded-full cursor-pointer hover:scale-110 hover:rotate-[20deg] transition-transform duration-[300ms] active:scale-50"
 
-@Input() acqua!:boolean
-@Input() fuoco!:boolean
-@Input() vento!:boolean
-@Input() terra!:boolean
-@Input() erba!:boolean
-@Input() legendary!:boolean
 
-@Output() attributoMostrato = new EventEmitter<{ [key: string]: boolean }>();
-@Output() attrEliminUno = new EventEmitter<{ [key: string]: boolean }>();
-@Output() attrEliminDue = new EventEmitter<{ [key: string]: boolean }>();
-@Output() attrEliminTre = new EventEmitter<{ [key: string]: boolean }>();
-@Output() attrEliminQuattro = new EventEmitter<{ [key: string]: boolean }>();
-@Output() attrEliminCinque = new EventEmitter<{ [key: string]: boolean }>();
-
-attributeChanger(attributo: string, eliminareUno: string, eliminareDue: string, eliminareTre: string, eliminareQuattro: string, eliminareCinque: string) {
-
-  (this as any)[attributo] = !(this as any)[attributo];
-
-
-  if ((this as any)[eliminareUno]) {
-    (this as any)[eliminareUno] = false;
-  }
-  if ((this as any)[eliminareDue]) {
-    (this as any)[eliminareDue] = false;
-  }
-  if ((this as any)[eliminareTre]) {
-    (this as any)[eliminareTre] = false;
-  }
-  if ((this as any)[eliminareQuattro]) {
-    (this as any)[eliminareQuattro] = false;
-  }
-  if ((this as any)[eliminareCinque]) {
-    (this as any)[eliminareCinque] = false;
-  }
-
-
-  this.attributoMostrato.emit((this as any)[attributo]);
-  this.attrEliminUno.emit((this as any)[eliminareUno]);
-  this.attrEliminDue.emit((this as any)[eliminareDue]);
-  this.attrEliminTre.emit((this as any)[eliminareTre]);
-  this.attrEliminQuattro.emit((this as any)[eliminareQuattro]);
-  this.attrEliminCinque.emit((this as any)[eliminareCinque]);
-
-}
 
 }
