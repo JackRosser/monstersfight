aef6a57f0e3e831a52ea427316d97932e2f2b7bf
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-16 21:46:04 +0200

    decklist

--- src/app/modals/deckmodal/deckmodal.component.ts
+++ src/app/modals/deckmodal/deckmodal.component.ts
@@ -1,7 +1,7 @@
+import { DeckService } from '../../services/deck.service';
 import { iMonsters } from './../../models/i-monsters';
 import { GlobalfetchService } from './../../services/globalfetch.service';
 import { Component, Input } from '@angular/core';
-
 
 @Component({
   selector: 'app-deckmodal',
@@ -10,12 +10,22 @@ import { Component, Input } from '@angular/core';
 })
 export class DeckmodalComponent {
 
+constructor(private decklist:DeckService) {}
 
-
-@Input() deckModal!: string
+@Input() deckModal!: string // DENTRO A NAVBAR
 
 deck!: iMonsters[]
 
+deleteCard(cardId:number) {
+  this.decklist.deleteCardInDeck(cardId).subscribe()
+}
+
+ngOnInit() {
+  this.decklist.loadDeck(); // MI aSSICURO CHE IL DECK SI CARICHI
+  this.decklist.cardsInDeck$.subscribe(list => {
+    this.deck = list;
+  });
+}
 
 
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -9,8 +9,8 @@ export class NavbarComponent {
 
 openCloseToggle: boolean = false
 deckImg: string = "services/icons/deck_close.png"
-modalOpen: string = "absolute w-full h-1/2 z-50 bottom-0 lg:end-0 lg:h-full lg:w-1/2 xl:w-2/3 overflow-auto bg-black"
-modalClose: string = "hidden absolute w-full h-1/2 z-50 bottom-0 lg:end-0 lg:h-full lg:w-1/2 xl:w-2/3 overflow-auto bg-black"
+modalOpen: string = "absolute w-full h-1/2 z-50 bottom-0 lg:end-0 lg:h-full lg:w-1/2 xl:w-2/3 overflow-auto bg-black grid grid-cols-2 gap-3 p-3 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-3"
+modalClose: string = "hidden absolute w-full h-1/2 z-50 bottom-0 lg:end-0 lg:h-full lg:w-1/2 xl:w-2/3 overflow-auto bg-black grid grid-cols-2 gap-3 p-3 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-3"
 deckModal: string = this.modalClose
 
 toggleChange(par: boolean) {
