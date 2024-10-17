a695550dcd442986e045f19c38182c0fe2288a35
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-17 20:04:54 +0200

    dimensioni giuste

--- src/app/combat/opponent/opponent.component.ts
+++ src/app/combat/opponent/opponent.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AllcardsService } from '../../services/allcards.service';
+import { iMonsters } from '../../models/i-monsters';
 
 @Component({
   selector: 'app-opponent',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrl: './opponent.component.scss'
 })
 export class OpponentComponent {
+
+  constructor(private chiamataAll:AllcardsService) {}
+
+  opponentCards: iMonsters[] = []
+
+  monsterActive!: iMonsters
+  background:string = ""
+  hpCounter: number = 100
+  hp:string = `width: ${this.hpCounter}%`
+  staminaCounter: number = 100
+  stamina:string = `width: ${this.staminaCounter}%`
+
+  ngOnInit() {
+
+    this.chiamataAll.allCards$.subscribe(allCards => {
+    let randomIndex:number = Math.floor(Math.random() * allCards.length)
+    this.opponentCards = allCards
+    this.monsterActive = this.opponentCards[randomIndex]
+    this.background = `background: url(${this.monsterActive.sfondo}); background-position: center; background-size: cover`
+
+  })
+  }
+
 
 }
