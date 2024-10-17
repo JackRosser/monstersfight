d24e65c201ad6c0839f8cf5dc2a66348760463b0
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-17 20:43:02 +0200

    animazione

--- src/app/combat/player/player.component.ts
+++ src/app/combat/player/player.component.ts
@@ -20,11 +20,29 @@ hp:string = `width: ${this.hpCounter}%`
 staminaCounter: number = 100
 stamina:string = `width: ${this.staminaCounter}%`
 
+battleAnimation: string = "none";
+toggleAnimation: boolean = false;
+
+battle() {
+
+  this.battleAnimation = "none";
+  this.toggleAnimation = true;
+
+setTimeout(() => {
+    if (this.toggleAnimation) {
+      this.battleAnimation = "battle 500ms ease-in-out";
+      this.toggleAnimation = !this.toggleAnimation;
+    }
+  }, 10);
+}
+
+
+
 ngOnInit() {
 this.chiamataDeck.deck$.subscribe(deckGlobal => {
   this.deckPlayer = deckGlobal
   this.monsterActive = this.deckPlayer[0]
-  this.background = `background: url(${this.monsterActive.sfondo}); background-position: center; background-size: cover`
+  this.background = `url(${this.monsterActive.sfondo})`
 
 })
 }
