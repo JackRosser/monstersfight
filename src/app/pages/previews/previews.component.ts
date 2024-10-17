ab683d58a2787fb78e93475c8fdccb6b3a8a5b6f
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-17 10:01:07 +0200

    funziona il bordo al click e l'add al deck

--- src/app/pages/previews/previews.component.ts
+++ src/app/pages/previews/previews.component.ts
@@ -31,16 +31,16 @@ monsterHover(monster: iMonsters) {
 newCard!: iMonsters
 
 //GESTIONE TOGGLE
-toggleCardInDeck: boolean = false;
+
 alertMostrato: string = "absolute bg-black p-5 w-2/3 md:w-1/4 lg:top-[5rem] text-white rounded-[10px] z-50 flex flex-col items-center gap-5 border shadow-sm";
 alert: string = "hidden";
 alertAttivo: boolean = false;
 
 
 
-cardInDeck(monster: iMonsters, cardId: number) {
-  if (!this.toggleCardInDeck) {
-    this.toggleCardInDeck = true;
+cardInDeck(monster: iMonsters, cardId: number, inDeck:boolean) {
+
+  if (inDeck = true) {
     this.deck.addCardToDeck(monster).subscribe({
       error: () => {
         this.alert = this.alertMostrato;
@@ -50,7 +50,6 @@ cardInDeck(monster: iMonsters, cardId: number) {
       }
     });
   } else {
-    this.toggleCardInDeck = false;
     this.deck.deleteCardInDeck(cardId).subscribe();
   }
 }
