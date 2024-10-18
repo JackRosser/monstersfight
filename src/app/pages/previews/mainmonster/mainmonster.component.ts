8fb0f220db6d0ff9d84ac8147ab7ca70e116c08c
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-18 07:58:36 +0200

    fix errori inspector

--- src/app/pages/previews/mainmonster/mainmonster.component.ts
+++ src/app/pages/previews/mainmonster/mainmonster.component.ts
@@ -15,9 +15,26 @@ constructor(private cardsList:AllcardsService) {}
 // ASPETTO CHE MOSTROATTIVO CI SIA PRIMA di CAMBIARE MONSTERACTIVE
 
 monstersList!: iMonsters[]
-monsterActive!: iMonsters
+monsterActive: iMonsters = {
+  id: 0,
+  name: '',
+  type: '',
+  description: '',
+  principale: '',
+  debolezza: '',
+  hp: 0,
+  atk: 0,
+  def: 0,
+  speed: 0,
+  stamina: 0,
+  img: '',
+  icon: '',
+  locked: false,
+  indeck: false,
+  sfondo: ''
+}
 
-@Input() set mostroAttivo(value: iMonsters) {
+@Input() set mostroAttivo(value: iMonsters | null) {
   if (value) {
     this.monsterActive = value;
   }
@@ -25,10 +42,12 @@ monsterActive!: iMonsters
 
 ngOnInit() {
   this.cardsList.allCards$.subscribe(list => {
-    this.monstersList = list
-    this.monsterActive = this.monstersList[0]
-
-  })
+    this.monstersList = list;
+    if (this.monstersList.length > 0) {
+      this.monsterActive = this.monstersList[0];
+    }
+  });
 }
+
 
 }
